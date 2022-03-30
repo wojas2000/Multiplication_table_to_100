b384f6f0b5750e987c3825b3131c6ffe3add11bd
wojas2000 <wojas2000@o2.pl>
2022-03-30 15:47:00 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Multiplication_table_to_100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j = 0;
+
+            Console.WriteLine("Program mnoży liczby z pierwszego wiersza przez liczby z pierwszej kolumny i zestawia wyniki w formie tabelarycznej.");
+
+            //i - kolumny
+            for (i = 1; i <= 10; i++)
+            {
+                //j - wiersze
+                for (j = 1; j <= 10; j++)
+                {
+                    Console.Write(" \t {0}", i * j);
+                }
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}
